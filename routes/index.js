7af6c98d72497100229f002dbc320341b0e7ea55
민상연 <judemin2087@naver.com>
2020-08-14 15:46:35 +0900

    feat : puppeteer function, test crawling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const { getConnection } = require('typeorm');
+const puppeteer = require('puppeteer');
+
 const Assign = require('../schemas/assign');
+const Subject = require('../schemas/subject');
+
+const AssignMD = require('../models/assign').Assign;
+const SubjectMD = require('../models/subject').Subject;
 
 router.get('/', function(req, res, next) {
 	const connection = getConnection();
@@ -11,14 +17,30 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-router.post('/', function(req, res, next) {
-	const newChannel = req.body;
-	const connection = getConnection();
-	const repository = connection.getRepository(Assign.options.name);
-	repository.save({
-		channelName: newChannel.name,
-	});
-	res.status(201).json();
+// 사용자의 계정으로 로그인 필요
+// http://ecampus.konkuk.ac.kr/ilos/main/main_form.acl
+router.get('/crawler', function(req, res, next) {
+	getSubjects("http://ecampus.konkuk.ac.kr/ilos/main/main_form.acl");
+	res.status(200).json("Crawler Initiated");
 });
+
+let subjectInfo = [];
+
+async function getSubjects(url){
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+  
+	await page.goto(url);
+
+	await page.waitForSelector("div", {timeout: 10000});
+
+	const result = await page.evaluate(() => {
+		const tData = Array.from(document.querySelectorAll("em"));
+		tData.flat(Infinity);
+
+		return tData;
+	});
+	console.log(result);
+}
 
 module.exports = router;
